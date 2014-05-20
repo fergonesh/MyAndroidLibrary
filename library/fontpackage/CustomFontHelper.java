ba944bb83c3b9d723cfc2025295dc776186b0039
Pavlo Shengofer <pashashenhofer@gmail.com>
2014-05-20 18:17:45 +0300

    add external font for Button and TextView;

---
+++ library/fontpackage/CustomFontHelper.java
@@ -0,0 +1,42 @@
+package library.fontpackage;
+
+import ibiapi.richbastard.R;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class CustomFontHelper {
+
+    /**
+     * Sets a fontpackage on a textview based on the custom fontpackage attribute
+     * If the custom fontpackage attribute isn't found in the attributes nothing happens
+     * @param textview
+     * @param context
+     * @param attrs
+     */
+    public static void setCustomFont(TextView textview, Context context, AttributeSet attrs) {
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.CustomFont);
+        String font = a.getString(R.styleable.CustomFont_font);
+        setCustomFont(textview, font, context);
+        a.recycle();
+    }
+
+    /**
+     * Sets a fontpackage on a textview
+     * @param textview
+     * @param font
+     * @param context
+     */
+    public static void setCustomFont(TextView textview, String font, Context context) {
+        if(font == null) {
+            return;
+        }
+        Typeface tf = FontCache.get(font, context);
+        if(tf != null) {
+            textview.setTypeface(tf);
+        }
+    }
+
+}
